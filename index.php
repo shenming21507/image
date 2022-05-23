005067d1d9af709a31c48057c3f27905a8cd29c5
shenming21507 <106001008+shenming21507@users.noreply.github.com>
2022-05-23 10:33:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,41 @@
 <?php
-//存有美图链接的文件名img.txt
-$filename = "img.txt";
-if(!file_exists($filename)){
-    die('文件不存在');
-}
- 
-//从文本获取链接
-$pics = [];
-$fs = fopen($filename, "r");
-while(!feof($fs)){
-    $line=trim(fgets($fs));
-    if($line!=''){
-        array_push($pics, $line);
+error_reporting(E_ERROR);
+require_once 'imgdata.php';
+$karnc = new imgdata();
+
+/**
+ * 遍历获取目录下的指定类型的文件
+ * @param $path
+ * @param array $files
+ * @return array
+ */
+function getfiles($path, $allowFiles, &$files = array()) {
+    if (!is_dir($path)) return null;
+    if (substr($path, strlen($path) - 1) != '/') $path .= '/';
+    $handle = opendir($path);
+    while (false !== ($file = readdir($handle))) {
+        if ($file != '.' && $file != '..') {
+            $path2 = $path . $file;
+            if (is_dir($path2)) {
+                getfiles($path2, $allowFiles, $files);
+            } else {
+                if (preg_match("/\.(" . $allowFiles . ")$/i", $file)) {
+                    $files[] = substr($path2, strlen($_SERVER['DOCUMENT_ROOT']));
+                }
+            }
+        }
     }
+    return $files;
 }
- 
-//从数组随机获取链接
-$pic = $pics[array_rand($pics)];
- 
-//返回指定格式
-$type=$_GET['type'];
-switch($type){
- 
-//JSON返回
-case 'json':
-    header('Content-type:text/json');
-    die(json_encode(['pic'=>$pic]));
- 
-default:
-    die(header("Location: $pic"));
-}
- 
-?>
+
+/**
+ * 域名白名单校验函数
+ * @param $domain_list
+ * @return true/false
+ */
+function checkReferer($domain_list = array(
+    'haremu.com',
+    'acg.sx'
+)) {
+    $status = false;
+    $refer
